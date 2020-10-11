5cbc8ff26df74511ff21056837eac98be1a4cd38
Takahiro Abe <hirotaka081630@gmail.com>
2020-10-11 11:33:58 +0900

    p.286

--- 8.rb
+++ 8.rb
@@ -1,34 +1,29 @@
 # 第８章モジュールを理解する
 require 'pry'
 
-class Team
-  COUNTRIES = ['Japan'.freeze, 'US'.freeze, 'India'.freeze].freeze
-end
-
-class Product
-  def title
-    log 'title is callded.'
-    'A great movie'
-  end
-
-  private
-
+module Loggable
   def log(text)
     puts "[LOG] #{text}"
   end
 end
 
+class Product
+  include Loggable
+
+  def title
+    log 'title is callded.'
+    'A great movie'
+  end
+end
+
 class User
+  include Loggable
+
   def name
     log 'name is called.'
     'Alice'
   end
 
-  private
-
-  def log(text)
-    puts "[LOG] #{text}"
-  end
 end
 product = Product.new
 product.title
