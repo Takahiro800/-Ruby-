170e4e89f668fda363b9e11dcc6123dd9102bae9
Takahiro Abe <hirotaka081630@gmail.com>
2020-10-11 14:09:14 +0900

    p292 8.4.3 deep_freezeメソッドを実装する

--- test/deep_freezable_test.rb
+++ test/deep_freezable_test.rb
@@ -1,8 +1,14 @@
 require 'minitest/autorun'
-require './lib/deep_freezable'
+require './lib/bank'
+require './lib/team'
 
 class DeepFreezableTest < Minitest::Test
-  def test_deep_freeze
-    assert DeepFreezable
+  def test_deep_freeze_to_array
+    assert_equal ['Japan', 'US', 'India'], Team::COUNTRIES
+
+    #配列自身がfreezeされているか？
+    assert Team::COUNTRIES.frozen?
+    #配列の要素がすべてfreezeされているか？
+    assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
 end
