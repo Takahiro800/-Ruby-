cdfe4216a7efa6361092f9e8a371cd37e91336e9
Takahiro Abe <hirotaka081630@gmail.com>
2020-10-12 21:13:11 +0900

    p225 Gateクラスにenterメソッドを定義

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,9 +1,15 @@
 require 'minitest/autorun'
 require './lib/gate'
+require './lib/ticket'
 
 class GateTest < MiniTest::Test
   def test_gate
-
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    # 「湯津城できる」という結果を期待しているので、juso.exit(ticket) の戻り値はtrueになるはず。
+    # それを検証するために最後にassertメソッドで戻り値がtrueになることを検証している
+    assert juso.exit(ticket)
   end
 end
